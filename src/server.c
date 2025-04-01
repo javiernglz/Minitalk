be71cd4e8b699147794b357b485cadd3e28c7519
Francisco Javier Navarrete Gonzalez <frnavarr@c2r3s2.42malaga.com>
2025-04-01 19:48:44 +0200

    server

--- src/server.c
+++ src/server.c
@@ -6,7 +6,16 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:14:24 by frnavarr          #+#    #+#             */
-/*   Updated: 2025/03/25 19:14:25 by frnavarr         ###   ########.fr       */
+/*   Updated: 2025/04/01 18:38:56 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	server(int signal)
+{
+	static int	i;
+	static int	c;
+
+	if (signal == SIGUSR1)
+		c=c | (1 << i);
+	i++;
+}
